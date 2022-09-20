7035f93f47577b4d3e824c2919d0ce3d83351851
nydina <63366733+nydina@users.noreply.github.com>
2022-09-20 10:28:24 +0200

    Initial Commit

---
+++ MyMoovies/MyMooviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMooviesApp.swift
+//  MyMoovies
+//
+//  Created by Dina Andrianarijaona on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMooviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
